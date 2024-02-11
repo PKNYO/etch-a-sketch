f8ca79433d53dbd8e5f84c34da76c591ffbe7dae
PKNYO <147757429+PKNYO@users.noreply.github.com>
2024-02-11 19:08:59 +0100

    Grid bug with change grid size
    Change the value always return the error popup

---
+++ main.js
@@ -0,0 +1,48 @@
+const container = document.querySelector(".container")
+const changeButton = document.querySelector(".change-button")
+const resetButton = document.querySelector(".reset-button")
+
+let gridSize = 16
+
+function createGrid(size) {
+    for (let i = 1; i <= size; i++) {
+        const row = document.createElement("div")
+    
+        row.classList.add("row")
+        container.appendChild(row)
+    
+        for (let j = 1; j <= size; j++) {
+            const square = document.createElement("div")
+    
+            square.classList.add("square")
+            row.appendChild(square)
+    
+            square.addEventListener("mouseover", (e) => {
+                e.target.classList.add("hover")
+            })
+        }
+    }
+}
+
+createGrid(gridSize)
+
+resetButton.addEventListener("click", () => {
+    const squares = document.querySelectorAll(".square")
+
+    squares.forEach((square) => {
+        square.classList.remove("hover")
+    })
+})
+
+changeButton.addEventListener("click", () => {
+    gridSize = prompt("Give a size between 5 and 100.")
+
+    if (gridSize > 100 || gridSize < 5 || gridSize !== typeof "number") {
+        return alert("Give a number between 5 and 100.")
+    }
+
+    container.innerHTML = ""
+
+    createGrid(gridSize)
+})
+
